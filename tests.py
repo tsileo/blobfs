916e513c05bd99b36c0f68d7013ca08ec38ebdc9
Thomas Sileo <thomas.sileo@gmail.com>
2016-11-04 00:36:25 +0100

    Improve tests

--- tests.py
+++ tests.py
@@ -1,147 +1,199 @@
+# coding: utf-8
 import os
 import random
 import string
 import time
 import hashlib
-from subprocess import Popen, PIPE
+import shutil
+from subprocess import Popen, PIPE, check_output
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+FILE_SIZE = 5 * 2 ** 20  # 5MB file by default
 
 
-def random_name():
-    return ''.join([random.choice(string.ascii_lowercase) for c in xrange(10)])
+def random_name(l=10):
+    """Returns a random string."""
+    # TODO(tsileo): update the choice to all the accepted chars for a filename
+    return ''.join([random.choice(string.ascii_lowercase) for c in xrange(l)])
 
-path = os.path.dirname(os.path.realpath(__file__))
 
-fs_name = random_name()
+class BlobFS(object):
+    def __init__(self, rebuild=True):
+        self.process = None
+        self.fs_name = random_name()
+        os.makedirs(self.fs_name)
+
+        self.mnt = os.path.join(PATH, self.fs_name)
+        if rebuild:
+            self._build()
+
+    def _build(self):
+        """Build the `blobsfs-mount` binary in the CWD"""
+        p = Popen(['go', 'build', './cmd/blobfs-mount/blobfs-mount.go'], env=os.environ)
+        if p.wait():
+            raise Exception('failed')
+
+    def mount(self):
+        """Execute `blobsfs-mount {fs_name} {fs_name}` and return the running process."""
+        self.process = Popen(['./blobfs-mount', '-loglevel', 'debug', self.fs_name, self.fs_name])
+        time.sleep(1)
+        if self.process.poll():
+            raise Exception('failed to mount')
+
+    def cleanup(self):
+        """Cleanup func."""
+        try:
+            os.remove('/tmp/blobfs_{}.sock'.format(self.fs_name))
+        except:
+            pass
+
+    def unmount(self):
+        # Unmount the FS if it's currently mounted
+        if self.process:
+            self.process.terminate()
+            self.process.wait()
+
+
+class File(object):
+    def __init__(self, path, hexhash):
+        self.path = path
+        self.hexhash = hexhash
+        self.basename = os.path.basename(path)
+
+    @staticmethod
+    def _write_random_data(path, size=FILE_SIZE):
+        """Open the file and write random data.
+
+        Returns the hexhash.
+        """
+        content = os.urandom(size)
+        h = hashlib.sha1()
+        h.update(content)
+        hexhash = h.hexdigest()
+
+        with open(path, 'wb+') as f:
+            f.write(content)
+
+        return hexhash
+
+    @classmethod
+    def from_random(cls, path, size=FILE_SIZE):
+        """Create a file filled with random data of the given size."""
+        hexhash = cls._write_random_data(path, size)
+        return cls(path, hexhash)
+
+    def edit(self, size=FILE_SIZE):
+        """Update the file content with random data and update the hexhash."""
+        self.hexhash = self._write_random_data(self.path, size)
+
+
+    def read_and_check(self):
+        """Read the file and ensure the hexhash matches."""
+        with open(self.path, 'rb') as f:
+            content = ''
+            h = hashlib.sha1()
+            while 1:
+                buf = f.read()
+                h.update(buf)
+                content += buf
+                if not buf:
+                    break
+            hexhash = h.hexdigest()
+
+        assert self.hexhash == hexhash
+
+    def remove(self):
+        """Remove the file."""
+        os.remove(self.path)
+
+    def move(self, path):
+        """Move the file to the given path (abs path expected)."""
+        if not os.path.isabs(path):
+            raise Exception('path for move op must be abosulte, got %{}'.format(path))
+
+        os.rename(self.path, path)
+        self.path = path
+        self.basename = os.path.basename(self.path)
+
+class Dir(object):
+    def __init__(self, path):
+        self.path = path
+        self.basename = os.path.basename(path)
+
+    def list(self):
+        """Returns the directory content."""
+        return os.listdir(self.path)
+
+    def remove(self):
+        """Remove the directory tree."""
+        shutil.rmtree(self.path)
+
+    def move(self, path):
+        """Move the file to the given path (abs path expected)."""
+        if not os.path.isabs(path):
+            raise Exception('path for move op must be abosulte, got %{}'.format(path))
+
+        os.rename(self.path, path)
+        self.path = path
+        self.basename = os.path.basename(self.path)
+
+    def create_dir(self, name=None):
+        if name is None:
+            name = random_name()
+        path = os.path.join(self.path, name)
+        os.mkdir(path)
+        return Dir(path)
+
+    def create_file(self, name=None, size=FILE_SIZE):
+        if name is None:
+            name = random_name()
+        path = os.path.join(self.path, name)
+        return File.from_random(path)
+
+blobfs = BlobFS()
+blobfs.cleanup()
+
+mnt = blobfs.mnt
+mnt = blobfs.mnt
+blobfs.mount()
 
 try:
-    os.remove('/tmp/blobfs_{}.sock'.format(fs_name))
-except:
-    pass
+    root_dir = Dir(mnt)
 
-os.makedirs(fs_name)
+    # Ensure the directory is empty
+    assert len(root_dir.list()) == 0
 
-mnt = os.path.join(path, fs_name)
+    # Create a file
+    f1 = root_dir.create_file()
+    # Ensure we can see the newly created file in the root dir
+    assert root_dir.list() == [f1.basename]
 
+    f1.read_and_check()
 
-def run_blob_cmd(cmd):
-    process = Popen(['blobfs', cmd], stdout=PIPE, cwd=mnt)
-    output = process.communicate()[0]
-    retcode = process.poll()
-    if retcode:
-        raise Exception('failed')
-    return output
+    d1 = root_dir.create_dir()
 
+    assert len(root_dir.list()) == 2
 
-def build_blobfs_mount():
-    p = Popen(['go', 'build', './cmd/blobfs-mount/blobfs-mount.go'], env=os.environ)
-    if p.wait():
-        raise Exception('failed')
+    # Check that we get EEXIST (17) error when creating a file that already exists
+    try:
+        root_dir.create_dir(name=d1.basename)
+    except OSError as exc:
+        assert exc.errno == 17
 
+    f2 = d1.create_file()
 
-# TODO(tsileo): build blobfs-mount
-def run_blobfs_mount():
-    p = Popen(['./blobfs-mount', '-loglevel', 'debug', fs_name, fs_name])
-    time.sleep(1)
-    if p.poll():
-        raise Exception('failed to mount')
-    return p
+    f2.read_and_check()
 
-build_blobfs_mount()
+    f2.move(os.path.join(mnt, random_name()))
 
-p = run_blobfs_mount()
-try:
+    f2.read_and_check()
 
-    contents = os.listdir(mnt)
+    f2.move(os.path.join(mnt, d1.basename, random_name()))
 
-    print 'contents='
-    print contents
-    print
-
-    rdir = random_name()
-
-    d1 = os.path.join(mnt, rdir)
-
-    os.mkdir(d1)
-    assert os.path.isdir(d1)
-
-    f1 = os.path.join(d1, random_name())
-    print 'f1 =', f1
-
-    f2 = os.path.join(mnt, random_name())
-    print 'f2 =', f2
-
-    f3 = os.path.join(d1, random_name())
-    print 'f3 =', f3
+    f2.read_and_check()
 
 
-    content = os.urandom(50 * 1024)
-
-    h = hashlib.sha1()
-    h.update(content)
-    hexhash = h.hexdigest()
-
-    with open(f1, 'wb+') as f:
-        f.write(content)
-
-    with open(f3, 'wb+') as f:
-        f.write(content)
-
-    with open(f1, 'rb') as f:
-        content2 = ''
-        h2 = hashlib.sha1()
-        while 1:
-            buf = f.read()
-            h2.update(buf)
-            content2 += buf
-            if not buf:
-                break
-        hexhash2 = h2.hexdigest()
-        assert hexhash == hexhash2
-        print 'Write test succeed'
-
-    os.rename(f1, f2)
-
-    with open(f2, 'rb') as f:
-        content3 = ''
-        h3 = hashlib.sha1()
-        while 1:
-            buf = f.read()
-            h3.update(buf)
-            content3 += buf
-            if not buf:
-                break
-        hexhash3 = h3.hexdigest()
-        assert hexhash == hexhash3
-        print 'Move test succeed'
-
-    os.remove(f2)
-    assert not os.path.isfile(f2)
-    print 'Delete test succeed'
-
-    # Unmount the FS, remount it and check the files are still there
-    p.terminate()
-    p.wait()
-    os.remove('/tmp/blobfs_{}.sock'.format(fs_name))
-    p = run_blobfs_mount()
-
-    print 'Running blobfs log'
-    print run_blob_cmd('log')
-    print 'ok'
-
-    # TODO(tsileo): remove cache and re-load?
-    with open(f3, 'rb') as f:
-        content3 = ''
-        h3 = hashlib.sha1()
-        while 1:
-            buf = f.read()
-            h3.update(buf)
-            content3 += buf
-            if not buf:
-                break
-        hexhash3 = h3.hexdigest()
-        assert hexhash == hexhash3
-        print 'Unmount/mount test succeed'
+    # from IPython import embed; embed()
 
 finally:
-    p.terminate()
-    p.wait()
+    blobfs.unmount()
