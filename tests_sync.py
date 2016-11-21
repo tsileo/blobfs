1e2388eafa5678b68c4fd5dbf4ff9757831219a9
Thomas Sileo <thomas.sileo@gmail.com>
2016-11-21 23:13:30 +0100

    Test the "added" merge conflict

--- tests_sync.py
+++ tests_sync.py
@@ -33,41 +33,81 @@ try:
     root_dir1 = Dir(mnt1)
     root_dir2 = Dir(mnt2)
 
-    f1_1 = root_dir1.create_file()
+    f1_1 = root_dir1.create_file(prefix='f1')
     f1_1.read_and_check()
+
+    assert len(root_dir1.list()) == 1
 
     print 'sync'
     print blobfs1.cmd('sync')
     print blobfs2.cmd('fetch')
 
+    assert len(root_dir2.list()) == 1
+
     f2_1 = File(os.path.join(mnt2, f1_1.basename), f1_1.hexhash)
     f2_1.read_and_check()
 
-    f2_2 = root_dir2.create_file()
+    f2_2 = root_dir2.create_file(prefix='f2')
     f2_2.read_and_check()
+
+    assert len(root_dir2.list()) == 2
 
     print blobfs2.cmd('sync')
     print 'FETCH'
+    # Back to mount 1
     print blobfs1.cmd('fetch')
+
+    assert len(root_dir1.list()) == 2
+
+    f1_1.read_and_check()
 
     f1_2 = File(os.path.join(mnt1, f2_2.basename), f2_2.hexhash)
     f1_2.read_and_check()
 
-    f1_3 = root_dir1.create_file()
+    f1_3 = root_dir1.create_file(prefix='f3')
     f1_3.read_and_check()
+
+    assert len(root_dir1.list()) == 3
 
     print blobfs1.cmd('sync')
 
-    f2_4 = root_dir2.create_file()
+    # Back to mount 2
+
+    f2_4 = root_dir2.create_file(prefix='f4')
     f2_4.read_and_check()
 
+    print 'MARK1', root_dir1.list()
+    print 'MARK2', root_dir2.list()
+
     print blobfs2.cmd('fetch')
+
+    f2_1.read_and_check()
+    f2_2.read_and_check()
+
+    print 'MARK21', root_dir2.list()
 
     f2_3 = File(os.path.join(mnt2, f1_3.basename), f1_3.hexhash)
     f2_3.read_and_check()
 
-    # TODO(tsileo): make a sync and ensure f2_4 can be read by fs1
+    assert len(root_dir2.list()) == 4
+#     os.remove(f2_1.path)
 
+    print 'SYNC'
+    print blobfs2.cmd('sync')
+    print 'FETCH'
+    print blobfs1.cmd('fetch')
+
+    f1_1.read_and_check()
+    f1_2.read_and_check()
+    f1_3.read_and_check()
+    f1_1.read_and_check()
+
+    print 'LEN', root_dir1.list(), f2_4.basename
+
+    f1_4 = File(os.path.join(mnt1, f2_4.basename), f2_4.hexhash)
+    f1_4.read_and_check()
+
+#     # FIXME(tsileo): ensure f1_1
 
 finally:
     blobfs1.unmount()
